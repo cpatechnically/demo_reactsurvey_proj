a3764104d776c9018d4ee2813ee9ed2f5b26e6b0
skccpa <kcallaway@ckcadvisors.com>
2021-04-12 22:48:07 -0500

    updates to build file

--- src/survey/hooks.js
+++ src/survey/hooks.js
@@ -1,17 +1,17 @@
 import React, {useState} from 'react'
 
 export const useInputChange = (customValue, callback) => {
-    const [value, setValue] = useState(customValue ? customValue : "")
-    const handleChange = event => {
-      const newValue = event.target.value
-      setValue(newValue)
-      if (callback) {
-        callback(event.target.name,newValue)
-      }
+  const [value, setValue] = useState(customValue ? customValue : "")
+  const handleChange = event => {
+    const newValue = event.target.value
+    setValue(newValue)
+    if (callback) {
+      callback(event.target.name,newValue)
     }
-    return  {
-      value: value,
-      handleChange: handleChange
-    }
-  
   }
+  return  {
+    value: value,
+    handleChange: handleChange
+  }
+
+}
